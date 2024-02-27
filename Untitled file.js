732b03f34a8b92750c98144a0c044d31bc5e7db9
HoangLong1806 <danghoanglong180602@gmail.com>
2024-02-27 08:51:08 +0700

    lan 1

---
+++ Untitled file.js
@@ -0,0 +1,65 @@
+import React, {useRef} from 'react';
+import {
+  Animated,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  SafeAreaView,
+} from 'react-native';
+
+const App = () => {
+  // fadeAnim will be used as the value for opacity. Initial Value: 0
+  d
+  const fadeIn = () => {
+    // Will change fadeAnim value to 1 in 5 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 5000,
+      useNativeDriver: true,
+    }).start();
+  };
+
+ 
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Animated.View
+        style={[
+          styles.fadingContainer,
+          {
+            // Bind opacity to animated value
+            opacity: fadeAnim,
+          },
+        ]}>
+        <Text style={styles.fadingText}>Fading View!</Text>
+      </Animated.View>
+      <View style={styles.buttonRow}>
+        <Button title="Fade In View" onPress={fadeIn} />
+       
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  fadingContainer: {
+    padding: 20,
+    backgroundColor: 'powderblue',
+  },
+  fadingText: {
+    fontSize: 28,
+  },
+  buttonRow: {
+    flexBasis: 100,
+    justifyContent: 'space-evenly',
+    marginVertical: 16,
+  },
+});
+
+export default App;
